54e82299160aacb9e19b87065ba0135ccf45ecd9
Eric Yang <eric@snapdocs.com>
2017-01-27 12:52:49 -0800

    Gobot

--- lita_config.rb
+++ lita_config.rb
@@ -1,6 +1,6 @@
 Lita.configure do |config|
   # The name your robot will use.
-  config.robot.name = "hackbot"
+  config.robot.name = "gobot"
 
   # The locale code for the language to use.
   config.robot.locale = :en
@@ -25,108 +25,7 @@ Lita.configure do |config|
   Lita.configure do |c|
     c.redis[:url] = ENV["REDIS_URL"]
     c.http.port = ENV["PORT"]
+    c.handlers.markov.database_url = ENV['DATABASE_URL']
   end
 
-  ## Example: Set configuration for any loaded handlers. See the handler's
-  ## documentation for options.
-  # config.handlers.some_handler.some_config_key = "value"
-  config.handlers.github_pinger.engineers = {
-    "Adam Derewecki" => {
-      usernames: {
-        slack: "adam",
-        github: "derwiki"
-      },
-      github_preferences: {
-        frequency: "all_discussion",
-        location: "dm"
-      },
-      travis_preferences: {
-        frequency: "only_failures"
-      }
-    },
-    "Evan Arnold" => {
-      usernames: {
-        slack: "evan",
-        github: "earnold"
-      },
-      github_preferences: {
-        frequency: "all_discussion",
-        location: "dm"
-      },
-      travis_preferences: {
-        frequency: "only_failures"
-      }
-    },
-    "Devin Powell" => {
-      usernames: {
-        slack: "devin",
-        github: "dpowell7"
-      },
-      github_preferences: {
-        frequency: "all_discussion",
-        location: "dm"
-      },
-      travis_preferences: {
-        frequency: "only_failures"
-      }
-    },
-    "Jake Moffatt" => {
-      usernames: {
-        slack: "jake",
-        github: "jakeonrails"
-      },
-      github_preferences: {
-        frequency: "all_discussion",
-        location: "dm"
-      },
-      travis_preferences: {
-        frequency: "only_failures"
-      }
-    },
-    "Alex Neigher" => {
-      usernames: {
-        slack: "alex.neigher",
-        github: "alexneigher"
-      },
-      github_preferences: {
-        frequency: "all_discussion",
-        location: "dm"
-      },
-      travis_preferences: {
-        frequency: "only_failures"
-      }
-    },
-    "Eric Yang" => {
-      usernames: {
-        slack: "eric",
-        github: "yangez"
-      },
-      github_preferences: {
-        frequency: "all_discussion",
-        location: "dm"
-      },
-      travis_preferences: {
-        frequency: "only_failures"
-      }
-    },
-    "Cristian Brotto" => {
-      usernames: {
-        slack: "cris",
-        github: "tycoool"
-      },
-      github_preferences: {
-        frequency: "all_discussion",
-        location: "dm"
-      },
-      travis_preferences: {
-        frequency: "only_failures"
-      }
-    },
-  }
-
-  Lita.configure do |config|
-    # For example, using Heroku and their Postgresql addon, the database can be
-    # configured by simply doing:
-    config.handlers.markov.database_url = ENV['DATABASE_URL']
-  end
 end
